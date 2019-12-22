919390d3d1f69db91cbac90e24985d019e1ac602
Zafir Hasan <zafirhsn@gmail.com>
2019-12-22 16:17:55 -0500

    :pencil: LeetCode ain't easy

--- Leetcode/add-two-numbers.js
+++ Leetcode/add-two-numbers.js
@@ -10,28 +10,39 @@
  * @param {ListNode} l2
  * @return {ListNode}
  */
-
-function ListNode(val) {
-    this.val = val;
-    this.next = null;
-}
-
-function addTwoNumbers(l1, l2) {
+var addTwoNumbers = function(l1, l2) {
   let carry = 0;
-  let final = [];
-  for (let i = 0; i < l1.length; i++) {
-    if (l1[i] + l2[i] >= 10) {
-      carry = 1;
-      final.push((l1[i] + l2[i]) % 10)
+  let final = new ListNode(0);
+  let pointerf = final;
+  let pointerl1 = l1;
+  let pointerl2 = l2;
+  let pval1 = 0;
+  let pval2 = 0;
+  while (pointerl1 !== null || pointerl2 !== null) {
+    if (pointerl1 !== null) {
+      pval1 = pointerl1.val;
+    } else { pval1 = 0; }
+    if (pointerl2 !== null) {
+      pval2 = pointerl2.val;
+    } else { pval2 = 0; }
+    
+    pointerf.next = new ListNode((pval1 + pval2 + carry) % 10);
+    carry = Math.floor((pval1 + pval2 + carry) / 10.0);
+    
+    if (pointerl1 !== null) { 
+      pointerl1 = pointerl1.next;
     }
-    else {
-      final.push(l1[i] + l2[i] + carry)
+    if (pointerl2 !== null) {
+      pointerl2 = pointerl2.next;
     }
+    pointerf = pointerf.next;
   }
-  return final;
+  if (carry === 1) {
+    pointerf.next = new ListNode(carry);
+  }
+  return final.next;
 };
 
-
 (function main() {
-
-}());
+  console.log("This is the correct answer for this problem");
+}())
---
+++ Leetcode/longest-substring-without-repeat.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+    
+};
+
+
+
+(function main(){
+  console.log("LeetCode problem 3 -- Longest Substring Without Repeating Characters");
+}())
+
+let hashT = {};
+console.log(hashT[" "]);
